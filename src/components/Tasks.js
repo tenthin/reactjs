a2c5218a272504988615e2f2d826cacfd841a539
Tenzin <tenthinten@gmail.com>
2021-12-22 15:12:24 +0530

    create list of tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,13 @@
-import { useState } from "react"
+import Task from './Task'
 
-const Task = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder:true,
-        },
-        {
-            id: 2,
-            text: 'Metting at School',
-            day: 'Feb 6th at 1:30pm',
-            reminder:true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder:false,
-        }
-    ])
-
-    return (
+const Tasks = ({tasks}) => {
+      return (
         <>
          {tasks.map((task) => (
-         <h3 key={task.id}>{task.text}</h3>
+         <Task key={task.id} task={task}/>
          ))}   
         </>
     )
 }
 
-export default Task
+export default Tasks
