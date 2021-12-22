7374d31ef5ef0af428faa278d58f5404cd4bc28c
Tenzin <tenthinten@gmail.com>
2021-12-22 14:56:48 +0530

    creating task in task.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
   return (
     <div className='Container'>
       <Header />
+      <Tasks />
     </div>
   )
 }
