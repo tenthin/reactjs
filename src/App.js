2ab00f1acc098215f4a150df4d3169c96cc10a07
Tenzin <tenthinten@gmail.com>
2021-12-23 17:34:55 +0530

    fetch task from server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -6,31 +6,29 @@ import AddTask from './components/AddTask'
 const App = () => {
   const[showAddTask, setShowAddTask] = useState
   (false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder:true,
-    },
-    {
-        id: 2,
-        text: 'Metting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder:true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder:false,
-    }
-])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+   const getTasks = async () => {
+     const tasksFromServer = await fetchTasks()
+     setTasks(tasksFromServer)
+   }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async() => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    console.log(data)
+    return data
+  }
 
 // Add Task
 const addTask = (task) => {
-  const id = Math.floor(Math.random() *
-  1000) + 1
+  const id = Math.floor(Math.random() * 1000) + 1
   const newTask = { id, ...task}
   setTasks([...tasks, newTask])
   console.log(task)
