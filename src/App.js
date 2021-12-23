5e6ccfec8adefc0d294950e7542728f547e7341c
Tenzin <tenthinten@gmail.com>
2021-12-24 00:11:57 +0530

    missing routes tag during import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState,useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Tasks from './components/Tasks'
@@ -7,15 +7,14 @@ import AddTask from './components/AddTask'
 import About from './components/About'
 
 const App = () => {
-  const[showAddTask, setShowAddTask] = useState
-  (false)
+  const[showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-   const getTasks = async () => {
-     const tasksFromServer = await fetchTasks()
-     setTasks(tasksFromServer)
-   }
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
 
     getTasks()
   }, [])
@@ -58,63 +57,71 @@ const App = () => {
   // setTasks([...tasks, newTask])
 }
 
-// Delete Task
+  // Delete Task
   const deleteTask = async (id) => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-    method:'DELETE',
-  })
+      method:'DELETE',
+    })
+    res.status === 200
+      ? setTasks(tasks.filter((task) => task.id !==id))
+      : alert('Error Deleting This Task')
+  }
 
-  setTasks(tasks.filter((task) => task.id !==id))
-}
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-// Toggle Reminder
-const toggleReminder = async (id) => {
-  const taskToToggle = await fetchTask(id)
-  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method:'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
 
-  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-    method:'PUT',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify(updTask),
-  })
-
-  const data = await res.json()
+    const data = await res.json()
 
 
-  setTasks(
-    tasks.map((task) =>
-      task.id === id ? { ...task, reminder: data.reminder } : task
-    )
-  ) 
-}
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    ) 
+  }
 
   return (
     <Router>
-    <div className='Container'>
-      <Header 
-        onAdd={() => setShowAddTask
-        (!showAddTask)} 
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete=
-        {deleteTask} onToggle={toggleReminder}
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
         />
-      ):( 
-        'No Tasks To Show'
-      )}
-      <Route path='/' exact  render={(props)
-        => (
-          
-        )} />
-      <Route path='/about' component={About} />
-      <Footer />
-    </div>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   )
 }
+
 
 export default App;
