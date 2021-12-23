993a7eefebc7e0d4aaa7b6eab48893d376ca20b7
Tenzin <tenthinten@gmail.com>
2021-12-23 17:47:20 +0530

    add task to server

--- src/App.js
+++ src/App.js
@@ -26,11 +26,23 @@ const App = () => {
   }
 
 // Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 1000) + 1
-  const newTask = { id, ...task}
-  setTasks([...tasks, newTask])
-  console.log(task)
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks',{
+    method:'POST',
+    headers:{
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random() * 1000) + 1
+  // const newTask = { id, ...task}
+  // setTasks([...tasks, newTask])
 }
 
 // Delete Task
